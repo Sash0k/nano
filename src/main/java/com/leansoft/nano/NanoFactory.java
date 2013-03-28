0134e75025574606ef752dfbeb12fa708315b17a
Александр Возженников <Sash0k.V@yandex.ru>
2013-03-28 13:27:22 +0400

    json implementation removed

--- src/main/java/com/leansoft/nano/NanoFactory.java
+++ src/main/java/com/leansoft/nano/NanoFactory.java
@@ -1,7 +1,5 @@
 package com.leansoft.nano;
 
-import com.leansoft.nano.impl.JsonReader;
-import com.leansoft.nano.impl.JsonWriter;
 import com.leansoft.nano.impl.XmlPullWriter;
 import com.leansoft.nano.impl.XmlSAXReader;
 
@@ -34,28 +32,7 @@ public class NanoFactory {
 	public static IReader getXMLReader(Format format) {
 		return new XmlSAXReader(format);
 	}
-	
-	/**
-	 * Get IReader instance with default format(encoding is utf-8),
-	 * the IReader instance cna be used to read JSON into POJO.
-	 * 
-	 * @return an instance of IReader implementation
-	 */
-	public static IReader getJSONReader() {
-		return new JsonReader();
-	}
-	
-	/**
-	 * Get IReader instance with specific format,
-	 * the IReader instance can be used to read JSON into POJO.
-	 * 
-	 * @param format info about encoding
-	 * @return an instance of IReader implementation.
-	 */
-	public static IReader getJSONReader(Format format) {
-		return new JsonReader(format);
-	}
-	
+
 	/**
 	 * Get IWriter instance with default format(encoding is utf-8, indent is true),
 	 * the IWriter instance can be used to write Java POJO into XML.
@@ -75,25 +52,5 @@ public class NanoFactory {
 	 */
 	public static IWriter getXMLWriter(Format format) {
 		return new XmlPullWriter(format);
-	}
-	
-	/**
-	 * Get IWriter instance with default format(encoding is utf-8),
-	 * the IWriter instance can be used to write Java POJO into JSON.
-	 * 
-	 * @return an instance of IWriter implementation
-	 */
-	public static IWriter getJSONWriter() {
-		return new JsonWriter();
-	}
-
-	/**
-	 * Get IWriter instance with specific format,
-	 * the IWriter instance can be used to write Java POJO into JSON.
-	 * 
-	 * @return an instance of IWriter implementation
-	 */
-	public static IWriter getJSONWriter(Format format) {
-		return new JsonWriter(format);
 	}
 }
